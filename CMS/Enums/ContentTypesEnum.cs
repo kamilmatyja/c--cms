eaf0a5cd68f187cde8fc9e35084bbbd90c386fd4
kamil-matyja <matyja_kamil@o2.pl>
2024-12-19 15:33:12 +0100

    Add enums

---
+++ CMS/Enums/ContentTypesEnum.cs
@@ -0,0 +1,7 @@
+namespace CMS.Enums;
+
+public enum ContentTypesEnum
+{
+    Text = 1,
+    Image = 2
+}
---
+++ CMS/Enums/UserRolesEnum.cs
@@ -0,0 +1,10 @@
+namespace CMS.Enums;
+
+public enum UserRolesEnum
+{
+    Reader = 1,
+    Analyst = 2,
+    Moderator = 3,
+    Author = 4,
+    Administrator = 5
+}
