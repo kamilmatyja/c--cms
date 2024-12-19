9fe2a3a8eb1b84b152a4b0eb9916bc6f23e9ec7a
kamil-matyja <matyja_kamil@o2.pl>
2024-12-19 15:53:06 +0100

    Update model

--- CMS/Models/EntryModel.cs
+++ CMS/Models/EntryModel.cs
@@ -4,11 +4,11 @@ public class EntryModel
 {
     public int Id { get; set; }
 
-    public int? UserId { get; set; }
-    public virtual UserModel User { get; set; }
-
     public int PageId { get; set; }
     public virtual PageModel Page { get; set; }
+
+    public int? UserId { get; set; }
+    public virtual UserModel User { get; set; }
 
     public DateTime CreatedAt { get; set; }
 }
